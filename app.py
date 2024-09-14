3b4ba3ec07d69376da6e51c30cd15fb90b47d546
Hoang-Dien-IT <nguyenhoangdien1x@gmail.com>
2024-09-14 14:37:53 +0700

    cap nhat lai giao dien

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request
 import numpy as np
 import joblib
-from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
 
 app = Flask(__name__)
 
 # Tải model và scaler đã lưu
 model = joblib.load('random_forest_model_rice.pkl')
-scaler = joblib.load('scaler_rice.pkl')  # scaler đã được huấn luyện trên tập dữ liệu gốc
+df_cleaned = pd.read_csv("../normalize_datasets/standardized-rice.csv")
+# scaler = joblib.load('scaler_rice.pkl')  # scaler đã được huấn luyện trên tập dữ liệu gốc
 
 @app.route('/')
 def home():
@@ -26,14 +27,31 @@ def predict():
         convex_area = float(request.form['convex_area'])
         extent = float(request.form['extent'])
 
+        Area = [df_cleaned['Area'].min(), df_cleaned['Area'].max()]
+        Perimeter = [df_cleaned['Perimeter'].min(), df_cleaned['Perimeter'].max()]
+        Major_Axis_Length = [df_cleaned['Major_Axis_Length'].min(), df_cleaned['Major_Axis_Length'].max()]
+        Minor_Axis_Length = [df_cleaned['Minor_Axis_Length'].min(), df_cleaned['Minor_Axis_Length'].max()]
+        Eccentricity = [df_cleaned['Eccentricity'].min(), df_cleaned['Eccentricity'].max()]
+        Convex_Area = [df_cleaned['Convex_Area'].min(), df_cleaned['Convex_Area'].max()]
+        Extent = [df_cleaned['Extent'].min(), df_cleaned['Extent'].max()]
+
+        area = (area - Area[0]) / (Area[1] - Area[0])
+        perimeter = (perimeter - Perimeter[0]) / (Perimeter[1] - Perimeter[0])
+        major_axis_length = (major_axis_length - Major_Axis_Length[0]) / (Major_Axis_Length[1] - Major_Axis_Length[0])
+        minor_axis_length = (minor_axis_length - Minor_Axis_Length[0]) / (Minor_Axis_Length[1] - Minor_Axis_Length[0])
+        eccentricity = (eccentricity - Eccentricity[0]) / (Eccentricity[1] - Eccentricity[0])
+        convex_area = (convex_area - Convex_Area[0]) / (Convex_Area[1] - Convex_Area[0])
+        extent = (extent - Extent[0]) / (Extent[1] - Extent[0])
+
         # Tạo mảng numpy từ dữ liệu đầu vào
+        scaled_data = []
         features = np.array([[area, perimeter, major_axis_length, minor_axis_length, eccentricity, convex_area, extent]])
-
-        # Áp dụng MinMaxScaler
-        features_scaled = scaler.transform(features)
-
+        print(features)
+        # # Áp dụng MinMaxScaler
+        # features_scaled = scaler.transform(features)
+        # print(features_scaled)
         # Dự đoán xác suất của từng lớp nhãn
-        probabilities = model.predict_proba(features_scaled)[0]
+        probabilities = model.predict_proba(features)[0]
         class_0_prob = probabilities[0] * 100
         class_1_prob = probabilities[1] * 100
 
