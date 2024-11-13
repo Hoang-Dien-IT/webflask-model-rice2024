06dec6b1d77e67afc8118aedf70de684f7c68432
Hoang-Dien-IT <nguyenhoangdien1x@gmail.com>
2024-11-13 13:02:41 +0700

    Done

--- app.py
+++ app.py
@@ -20,29 +20,25 @@ def predict():
     try:
         # Lấy dữ liệu từ form
         area = float(request.form['area'])
-        perimeter = float(request.form['perimeter'])
         major_axis_length = float(request.form['major_axis_length'])
         minor_axis_length = float(request.form['minor_axis_length'])
         eccentricity = float(request.form['eccentricity'])
         extent = float(request.form['extent'])
 
         Area = [df_cleaned['Area'].min(), df_cleaned['Area'].max()]
-        Perimeter = [df_cleaned['Perimeter'].min(), df_cleaned['Perimeter'].max()]
         Major_Axis_Length = [df_cleaned['Major_Axis_Length'].min(), df_cleaned['Major_Axis_Length'].max()]
         Minor_Axis_Length = [df_cleaned['Minor_Axis_Length'].min(), df_cleaned['Minor_Axis_Length'].max()]
         Eccentricity = [df_cleaned['Eccentricity'].min(), df_cleaned['Eccentricity'].max()]
         Extent = [df_cleaned['Extent'].min(), df_cleaned['Extent'].max()]
 
         area = (area - Area[0]) / (Area[1] - Area[0])
-        perimeter = (perimeter - Perimeter[0]) / (Perimeter[1] - Perimeter[0])
         major_axis_length = (major_axis_length - Major_Axis_Length[0]) / (Major_Axis_Length[1] - Major_Axis_Length[0])
         minor_axis_length = (minor_axis_length - Minor_Axis_Length[0]) / (Minor_Axis_Length[1] - Minor_Axis_Length[0])
         eccentricity = (eccentricity - Eccentricity[0]) / (Eccentricity[1] - Eccentricity[0])
         extent = (extent - Extent[0]) / (Extent[1] - Extent[0])
 
 
-        scaled_data = []
-        features = np.array([[area, perimeter, major_axis_length, minor_axis_length, eccentricity, extent]])
+        features = np.array([[area, major_axis_length, minor_axis_length, eccentricity, extent]])
         print(features)
 
         probabilities = model.predict_proba(features)[0]
